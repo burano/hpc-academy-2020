1e4356afa5d05aa1c8a82110f2073b12c202309d
burano <id583@york.ac.uk>
2020-09-09 10:42:30 +0100

    Update reversed_range.f90

--- fortran/practical2/reversed_range.f90
+++ fortran/practical2/reversed_range.f90
@@ -1,25 +1,34 @@
 program reversed_range
     implicit none
-    integer :: array10(10), i
-    integer :: lower_idx, upper_idx
+    integer, allocatable :: array(:)
+    integer :: size, i, lower_idx, upper_idx
 
-    do i = 1, 10
-        array10(i) = i
+    print*, "Enter size of the array:"
+    read*, size
+    if (size <= 0) then
+        print*, "Size must be greater than or equal to 1."
+        call exit()
+    end if
+
+    allocate(array(size))
+    do i = 1, size
+        array(i) = i
     end do
 
-    print*, array10
+    print*, array
 
     print*, "Enter lower and upper bounds: "
     read*, lower_idx, upper_idx
     
-    if (array10(1) < lower_idx .or. (lower_idx < array10(10)) .or. (upper_idx < array10(10)) .or. (array10(1) < upper_idx)) then
+    if (array(1) < lower_idx .or. (lower_idx < array(10)) .or. (upper_idx < array(10)) .or. (array(1) < upper_idx)) then
         if (lower_idx < upper_idx) then
-            print*, array10(lower_idx:upper_idx)
+            print*, array(lower_idx:upper_idx)
         else
-            print*, array10(lower_idx:upper_idx:-1)
+            print*, array(lower_idx:upper_idx:-1)
         end if
     else
-        print*, "Indices are out of range\nPlease enter two numbers with in [1,10] range."    
+        print*, "Indices are out of range\nPlease enter two numbers with in [1,10] range."
+        call exit()    
     end if
 
 end program reversed_range   
