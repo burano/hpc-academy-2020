380104c227d90446c86b9e465fc3896821763250
burano <id583@york.ac.uk>
2020-09-09 10:09:32 +0100

    Update reversed_range.f90

--- fortran/practical2/reversed_range.f90
+++ fortran/practical2/reversed_range.f90
@@ -11,6 +11,15 @@ program reversed_range
 
     print*, "Enter lower and upper bounds: "
     read*, lower_idx, upper_idx
+    
+    if (array10(1) < lower_idx .or. (lower_idx < array10(10)) .or. (upper_idx < array10(10)) .or. (array10(1) < upper_idx)) then
+        if (lower_idx < upper_idx) then
+            print*, array10(lower_idx:upper_idx)
+        else
+            print*, array10(lower_idx:upper_idx:-1)
+        end if
+    else
+        print*, "Indices are out of range\nPlease enter two numbers with in [1,10] range."    
+    end if
 
-    print*, array10(lower_idx:upper_idx)
 end program reversed_range   
