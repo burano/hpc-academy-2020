4aed2ec8c922b9bc7e078aeee6e05ebe0c36d38c
burano <id583@york.ac.uk>
2020-09-09 09:36:45 +0100

    Update reversed_range.f90

--- fortran/practical2/reversed_range.f90
+++ fortran/practical2/reversed_range.f90
@@ -1,10 +1,16 @@
 program reversed_range
     implicit none
     integer :: array10(10), i
+    integer :: lower_idx, upper_idx
 
-    do i=1,10
-        array10(i)=i
+    do i = 1, 10
+        array10(i) = i
     end do
 
     print*, array10
+
+    print*, "Enter lower and upper bounds: "
+    read*, lower_idx, upper_idx
+
+    print*, array10(lower_idx:upper_idx)
 end program reversed_range   
