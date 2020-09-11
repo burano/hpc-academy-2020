9218339a04200707dc6a3b008c51963184c6f605
Igor <burano@github.com>
2020-09-11 09:44:10 +0100

    Update cross_product

--- fortran/practical2/cross_product.f90
+++ fortran/practical2/cross_product.f90
@@ -4,15 +4,32 @@ program cross_product
 
     print*, "Vector 1: ", vector1
     print*, "Vector 2: ", vector2
-    print*, "Perform cross product..."
-    call multiply(vector1, vector2, vector_product)
-    print*, " Vector product: ", vector_product
-    
+
+    print*, "Call cross product subroutine..."
+    call multiply_subroutine(vector1, vector2, vector_product)
+    print*, "Vector product: ", vector_product
+
+    print*, "Call cross product function..."
+    print*, "Vector product: ", multiply_function(vector1, vector2)
+
 contains
-    subroutine multiply(x, y, z)
-        real, dimension(3) :: x, y, z
+    subroutine multiply_subroutine(x, y, z)
+        real, dimension(3), intent(in) :: x, y
+        real, dimension(3), intent(out) :: z
+
         z(1) = x(2) * y(3) - y(2) * x(3)
         z(2) = x(3) * y(1) - y(3) * x(1)
         z(3) = x(1) * y(2) - y(1) * x(2)
-    end subroutine multiply
+
+    end subroutine multiply_subroutine
+
+    function multiply_function(x, y) result(z)
+        real, dimension(3), intent(in) :: x, y
+        real, dimension(3) :: z
+
+        z(1) = x(2) * y(3) - y(2) * x(3)
+        z(2) = x(3) * y(1) - y(3) * x(1)
+        z(3) = x(1) * y(2) - y(1) * x(2)
+
+    end function multiply_function
 end program cross_product
