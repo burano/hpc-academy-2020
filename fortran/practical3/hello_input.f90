1b17d5a06179c2a5d1a67734622ee89dcb5fda84
burano <id583@york.ac.uk>
2020-09-11 10:14:21 +0100

    Create excercise 1

--- fortran/practical3/hello_input.f90
+++ fortran/practical3/hello_input.f90
@@ -1,7 +1,21 @@
 program hello_input
     implicit none
-    character(len=20) :: name, surname
-    print*, "How should I call you?"
-    read*, name, surname
-    print*, "Hello, ", name, " ", surname, "!"
+    character(len=20) :: name, age
+
+    print*, "Reading form a file..."
+    open(2, file = 'input.hello_input', status = 'old')
+    read(2,*) name, age
+    close(2)
+
+    ! print*, "Enter your name and age:"
+    ! read*, name, age
+    ! print*, "Hello, ", name, " ", age, "!"
+
+    print*, "Write to a file..."
+    open(1, file = 'output.hello_input', status = 'new')   
+    write(1,*) "Hello, ", name, " ", age, "!"
+    close(1)
+
+
+
 end program hello_input
