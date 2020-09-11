5fc7624911f2dfee9a5ebde7b881c855d157b846
burano <id583@york.ac.uk>
2020-09-11 10:26:09 +0100

    Update hello_input.f90

--- fortran/practical3/hello_input.f90
+++ fortran/practical3/hello_input.f90
@@ -1,9 +1,15 @@
 program hello_input
     implicit none
     character(len=20) :: name, age
+    character(len=30) :: input_file, output_file
+    
+    print*, "Enter input file name:"
+    read*, input_file
+    print*, "Enter output file name:"
+    read*, output_file
 
     print*, "Reading form a file..."
-    open(2, file = 'input.hello_input', status = 'old')
+    open(2, file = trim(input_file), status = 'old')
     read(2,*) name, age
     close(2)
 
@@ -12,10 +18,8 @@ program hello_input
     ! print*, "Hello, ", name, " ", age, "!"
 
     print*, "Write to a file..."
-    open(1, file = 'output.hello_input', status = 'new')   
-    write(1,*) "Hello, ", name, " ", age, "!"
+    open(1, file = trim(output_file), status = 'new')   
+    write(1,*) "Hello, ", trim(name), " - ", trim(age), "."
     close(1)
-
-
 
 end program hello_input
